e7062991793ce310dca9b6e4d25153e96aacc23d
Pavel Bortnik <pbortnik1207@gmail.com>
2024-02-26 22:45:02 +0300

    Initial commons-reporting lib commit

---
+++ src/main/java/com/epam/ta/reportportal/ws/reporting/FinishExecutionRQ.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.ws.reporting;
+
+import static com.epam.ta.reportportal.ws.reporting.ValidationConstraints.MAX_PARAMETERS_LENGTH;
+
+import com.epam.ta.reportportal.ws.annotations.In;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.Date;
+import java.util.Set;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Finishes some entity execution in Report Portal<br> May be Launch, TestSuite, Test, TestStep
+ *
+ * @author Andrei Varabyeu
+ */
+@JsonInclude(Include.NON_NULL)
+public class FinishExecutionRQ {
+
+  @NotNull
+  @JsonProperty(value = "endTime", required = true)
+  @JsonAlias({"endTime", "end_time"})
+  @ApiModelProperty(required = true)
+  private Date endTime;
+
+  @JsonProperty(value = "status")
+  @In(allowedValues = {"passed", "failed", "stopped", "skipped", "interrupted", "cancelled", "info",
+      "warn"})
+  @ApiModelProperty(allowableValues = "PASSED, FAILED, STOPPED, SKIPPED, INTERRUPTED, CANCELLED, INFO, WARN")
+  private String status;
+
+  @JsonProperty(value = "description")
+  private String description;
+
+  @Size(max = MAX_PARAMETERS_LENGTH)
+  @Valid
+  @JsonProperty
+  @JsonAlias({"attributes", "tags"})
+  private Set<ItemAttributesRQ> attributes;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Set<ItemAttributesRQ> getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(Set<ItemAttributesRQ> attributes) {
+    this.attributes = attributes;
+  }
+
+  public Date getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(Date endTime) {
+    this.endTime = endTime;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  @Override
+  public String toString() {
+    return "FinishExecutionRQ{" + "endTime=" + endTime
+        + ", status='" + status + '\''
+        + ", description='" + description + '\''
+        + ", attributes=" + attributes
+        + '}';
+  }
+}
---
+++ src/main/java/com/epam/ta/reportportal/ws/reporting/SaveLogRQ.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.ta.reportportal.ws.reporting;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.Arrays;
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Henadzi_Vrubleuski
+ * @author Andrei Varabyeu
+ */
+@JsonInclude(Include.NON_NULL)
+public class SaveLogRQ {
+
+  @JsonProperty("uuid")
+  private String uuid;
+
+  @JsonAlias({"itemUuid", "item_id"})
+  @ApiModelProperty(value = "UUID of test item owned this log")
+  private String itemUuid;
+
+  @JsonProperty(value = "launchUuid")
+  @ApiModelProperty(required = true)
+  private String launchUuid;
+
+  @NotNull
+  @JsonProperty(value = "time", required = true)
+  @ApiModelProperty(required = true)
+  private Date logTime;
+
+  @JsonProperty(value = "message")
+  private String message;
+
+  @JsonProperty(value = "level")
+  @ApiModelProperty(allowableValues = "error, warn, info, debug, trace, fatal, unknown")
+  private String level;
+
+  @JsonProperty(value = "file")
+  private File file;
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+  public Date getLogTime() {
+    return logTime;
+  }
+
+  public void setLogTime(Date logTime) {
+    this.logTime = logTime;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getItemUuid() {
+    return itemUuid;
+  }
+
+  public void setItemUuid(String itemUuid) {
+    this.itemUuid = itemUuid;
+  }
+
+  public String getLaunchUuid() {
+    return launchUuid;
+  }
+
+  public void setLaunchUuid(String launchUuid) {
+    this.launchUuid = launchUuid;
+  }
+
+  public void setLevel(String level) {
+    this.level = level;
+  }
+
+  public String getLevel() {
+    return level;
+  }
+
+  public void setFile(File file) {
+    this.file = file;
+  }
+
+  public File getFile() {
+    return file;
+  }
+
+  @JsonInclude(Include.NON_NULL)
+  public static class File {
+
+    @JsonProperty(value = "name")
+    private String name;
+
+    @JsonIgnore
+    private byte[] content;
+
+    @JsonIgnore
+    private String contentType;
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public byte[] getContent() {
+      return content;
+    }
+
+    public void setContent(byte[] content) {
+      this.content = content;
+    }
+
+    public String getContentType() {
+      return contentType;
+    }
+
+    public void setContentType(String contentType) {
+      this.contentType = contentType;
+    }
+
+    @Override
+    public String toString() {
+      return "File{" + "name='" + name + '\''
+          + ", content=" + Arrays.toString(content)
+          + ", contentType='" + contentType + '\''
+          + '}';
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "SaveLogRQ{" + "uuid='" + uuid + '\''
+        + ", itemUuid='" + itemUuid + '\''
+        + ", launchUuid='" + launchUuid + '\''
+        + ", logTime=" + logTime
+        + ", message='" + message + '\''
+        + ", level='" + level + '\''
+        + ", file=" + file
+        + '}';
+  }
+}

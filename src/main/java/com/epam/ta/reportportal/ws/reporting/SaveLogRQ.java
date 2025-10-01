629b7b1e8fd43bad64cfe9e417cdcf67bfb987ab
Evelina02 <evelina02.02.2000@gmail.com>
2025-10-01 18:18:12 +0300

    EPMRPP-108013 remove not null

--- src/main/java/com/epam/ta/reportportal/ws/reporting/SaveLogRQ.java
+++ src/main/java/com/epam/ta/reportportal/ws/reporting/SaveLogRQ.java
@@ -63,7 +63,6 @@ public class SaveLogRQ {
   @JsonProperty(value = "message")
   private String message;
 
-  @NotNull
   @JsonProperty(value = "level")
   @Size(min = 3, max = 16)
   @Pattern(regexp = "^[A-Za-z0-9 ]+$")
